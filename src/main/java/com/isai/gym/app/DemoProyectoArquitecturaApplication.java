3c1a5abf23d90c0c9a8df0f163fc3f7505d9bb80
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-05 22:32:21 -0500

    init

---
+++ src/main/java/com/isai/gym/app/DemoProyectoArquitecturaApplication.java
@@ -0,0 +1,13 @@
+package com.isai.gym.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProyectoArquitecturaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoProyectoArquitecturaApplication.class, args);
+    }
+
+}
