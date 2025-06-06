776df8046db99150ac4800c58f5c86fcdc65cbd2
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-06 00:48:57 -0500

    repositorio de usuario con los metodos para buscar por nombre y email

---
+++ src/main/java/com/isai/gym/app/repository/UsuarioRepository.java
@@ -0,0 +1,17 @@
+package com.isai.gym.app.repository;
+
+import com.isai.gym.app.entities.Usuario;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository
+        extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByNombreUsuario(@NotBlank(message = "El nombre de usuario no puede estar vacío") @Size(min = 4, max = 255, message = "El nombre de usuario debe tener entre 4 y 255 caracteres") String nombreUsuario);
+
+    Optional<Usuario> findByEmail(String email);
+}
