3eaaa8d061c541090b6b767e9fa58aa7601754f7
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-07 16:55:43 -0500

    UsuarioRepository: buscar usuarios por nombreCompleto o email, ignorando mayúsculas/minúsculas

--- src/main/java/com/isai/gym/app/repository/UsuarioRepository.java
+++ src/main/java/com/isai/gym/app/repository/UsuarioRepository.java
@@ -3,7 +3,11 @@ package com.isai.gym.app.repository;
 import com.isai.gym.app.entities.Usuario;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -14,4 +18,7 @@ public interface UsuarioRepository
     Optional<Usuario> findByNombreUsuario(@NotBlank(message = "El nombre de usuario no puede estar vacío") @Size(min = 4, max = 255, message = "El nombre de usuario debe tener entre 4 y 255 caracteres") String nombreUsuario);
 
     Optional<Usuario> findByEmail(String email);
+
+    Page<Usuario> findByNombreCompletoContainingIgnoreCaseOrEmailContainingIgnoreCase(String nombreCompleto, String email, Pageable pageable);
+
 }
