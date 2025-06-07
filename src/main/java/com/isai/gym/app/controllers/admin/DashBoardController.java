d7ee79f57547119c01c9c31cf037d19c57fa30b8
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-07 16:54:10 -0500

    controlador del dasborad para el admin

--- src/main/java/com/isai/gym/app/controllers/admin/DashBoardController.java
+++ src/main/java/com/isai/gym/app/controllers/admin/DashBoardController.java
@@ -1,15 +1,17 @@
 package com.isai.gym.app.controllers.admin;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(path = "/dashboard")
+@RequestMapping("/admin/dashboard")
+@PreAuthorize("hasRole('ADMIN')")
 public class DashBoardController {
 
-    @GetMapping(path = "")
+    @GetMapping("")
     public String mostrarDashBoard() {
-        return "dashboard";
+        return "admin/dashboard";
     }
 }
