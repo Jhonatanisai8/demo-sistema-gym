b2cd52156315db3ddbbd3a2eace062c91f1bf8fb
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-08 10:42:13 -0500

    repositorio para la entidad cliente

---
+++ src/main/java/com/isai/gym/app/repository/EntrenadorRepository.java
@@ -0,0 +1,23 @@
+package com.isai.gym.app.repository;
+
+import com.isai.gym.app.entities.Entrenador;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EntrenadorRepository
+        extends JpaRepository<Entrenador, Long> {
+
+    // metodo para buscar por nombre o especialidad
+    Page<Entrenador> findByNombreContainingIgnoreCaseOrEspecialidadContainingIgnoreCase(String nombre, String especialidad, Pageable pageable);
+
+    // método para verificar unicidad de nombre (opcional, pero útil para evitar duplicados)
+    Optional<Entrenador> findByNombre(String nombre);
+
+    // método para verificar unicidad de email (muy recomendable)
+    Optional<Entrenador> findByEmail(String email);
+}
