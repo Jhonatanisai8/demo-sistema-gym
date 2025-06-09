b0cb4551ba8d73b1cb512a7d4e3cf83619dc6e13
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-09 16:21:15 -0500

    repository para producto

---
+++ src/main/java/com/isai/gym/app/repository/ProductoRepository.java
@@ -0,0 +1,40 @@
+package com.isai.gym.app.repository;
+
+import com.isai.gym.app.entities.Producto;
+import com.isai.gym.app.enums.CategoriaProducto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+
+    Optional<Producto> findByNombreIgnoreCase(String nombre);
+
+    Optional<Producto> findByCodigoBarras(String codigoBarras);
+
+    @Query("SELECT p FROM Producto p WHERE " +
+            "LOWER(p.nombre) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.descripcion) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.categoria) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " + // <-- ¡CAMBIO AQUÍ!
+            "LOWER(p.codigoBarras) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.proveedor) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    Page<Producto> buscarProductosPorKeyword(@Param("keyword") String keyword, Pageable pageable);
+
+    Page<Producto> findByActivoTrue(Pageable pageable);
+
+    @Query("SELECT p FROM Producto p WHERE p.activo = true AND (" +
+            "LOWER(p.nombre) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.descripcion) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.categoria) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " + // <-- ¡CAMBIO AQUÍ!
+            "LOWER(p.codigoBarras) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.proveedor) LIKE LOWER(CONCAT('%', :keyword, '%')))")
+    Page<Producto> buscarProductosActivosPorKeyword(@Param("keyword") String keyword, Pageable pageable);
+
+    Page<Producto> findByCategoriaAndActivoTrue(CategoriaProducto categoria, Pageable pageable);
+}
